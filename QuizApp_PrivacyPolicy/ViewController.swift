b3d1296195a25a9e61f3a3527927609ed8faa040
JunShirai <j.shirai@proud-g.jp>
2021-12-04 22:07:29 +0900

    Initial Commit

---
+++ QuizApp_PrivacyPolicy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizApp_PrivacyPolicy
+//
+//  Created by 白井淳 on 2021/12/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
